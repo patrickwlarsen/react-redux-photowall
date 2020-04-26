d9a18d40f6b4e5110628c753a062fff63be2a7d8
Patrick W Larsen <patrickwlarsen@gmail.com>
2020-04-26 13:42:49 +0200

    Work in progress - Added stylesheet (copied from course) - Moved List component to plaground - Started working on the photowall

---
+++ photowall/src/components/PhotoWall.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PhotoWall extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default PhotoWall;
--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
+import './styles/stylesheet.css';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
